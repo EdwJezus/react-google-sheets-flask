28154ae030351ffa61497e04746caea1ae699b40
Eduardo Jesus <126268026+EdwJezus@users.noreply.github.com>
2026-06-29 13:03:17 -0300

    Atualização no Back para usar Conta de Serviço no Google

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from google_api import pega_planilha
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -37,7 +38,6 @@ def login_user():
 
 
 if __name__ == "__main__":
-    import os
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
